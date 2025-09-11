b4ed6188aaefc986a15083502e556b9103f68e15
Everaldo Deus <sgt_erd@hotmail.com>
2025-09-11 16:06:20 -0300

    commit3.0

--- SuperTrunfoMestre/Super_TrunfoMestre.c
+++ SuperTrunfoMestre/Super_TrunfoMestre.c
@@ -6,6 +6,7 @@ int main(){
     int pontosTuristicos;
     float area,populacao,PIB,DensidadePopulacional;
     float pibPerCapita;
+    float superPoder;
     char nome[30],estado[10] ;
     char codigo[4];
     
@@ -13,6 +14,7 @@ int main(){
     int pontosTuristicos2;
     float area2,populacao2,PIB2,DesnsidadePopulacional2;
     float pibPerCapita2;
+    float superPoder2;
     char nome2[30],estado2[10] ;
     char codigo2[4];
     
@@ -43,6 +45,7 @@ int main(){
     printf("///////////////////////////////////\n");   // Separador entre as cartas//
 
     // Entrada de dados da segunda carta//
+    
     printf("Isira os dados  da Segunda carta:\n");
     
     printf("Digite o nome da cidade: ");
@@ -72,6 +75,10 @@ int main(){
     PIB=PIB*1000000000;
     PIB2=PIB2*1000000000;
    
+    //calculo do super poder//
+        superPoder= (1/DensidadePopulacional);
+        superPoder2= (1/DesnsidadePopulacional2);
+
     //Calculo da densidade populacional//
       DensidadePopulacional= populacao/area;
       DesnsidadePopulacional2= populacao2/area2; 
@@ -87,10 +94,79 @@ int main(){
       PIB=PIB/1000000000;
       PIB2=PIB2/1000000000;
       
-    // Saida dos dados das cartas//
-
-    printf("\n------%s------\nCidade : %s\nEstado: %s\nCodigo: %s\nPopulacao: %.2f Habitantes\nArea: %.2fkm²\nPIB: %.2f Milhoes de reais\nPontos Turisticos: %d\nDensidade Demografica: %.2f hab/km²\nPIB per capita: %.2f Reais por habitante\n----------------\n",codigo,nome,estado,codigo,populacao,area,PIB,pontosTuristicos,DensidadePopulacional,pibPerCapita);
-    printf("\n------%s-------\nCidade : %s\nEstado: %s\nCodigo: %s\nPopulacao: %.2f Habitantes\nArea: %.2fkm²\nPIB: %.2f Milhoes de reais\nPontos Turisticos: %d\nDensidade Demografica: %.2f hab/km²\nPIB per capita: %.2f Reais por habitante\n-------------\n",codigo2,nome2,estado2,codigo2,populacao2,area2,PIB2,pontosTuristicos2,DesnsidadePopulacional2,pibPerCapita2);
+    /* Saida dos dados das cartas.
+    iserindo logica de comparacao junto com a saida das cartas.
+    (%d) ira apresentar 1 para "vencedor" e 0 para "perdedor".
+   */
     
+ 
+   //saida da carta 1//
+
+    printf("\n------%s------\n",codigo);
+
+    printf("Cidade: %s \n", nome);
+
+    printf("Estado: %s \n",  estado);
+
+    printf("Codigo: %s \n", codigo);
+
+    printf("Populacao: %.2f de Habitantes" , populacao);
+    printf("(%d)\n", populacao>=populacao2);
+
+    printf("Area: %.2f km² ", area);
+    printf("(%d)\n", area>=area2);
+
+    printf("PIB: %.2f Milhoes de reais ", PIB);
+    printf("(%d)\n", PIB>=PIB2);
+
+    printf("Pontos Turisticos: %d ", pontosTuristicos);
+    printf("(%d)\n", pontosTuristicos>=pontosTuristicos2);
+
+    printf("Densidade Demografica: %.2f hab/km² ", DensidadePopulacional);
+    printf("(%d)\n", DensidadePopulacional>=DesnsidadePopulacional2);
+
+    printf("PIB per capita: %.2f Reais por habitante ", pibPerCapita);
+    printf("(%d)\n", pibPerCapita>=pibPerCapita2);
+
+    printf("Super Poder: ");
+    printf("(%d)\n", superPoder>=superPoder2);
+
+    printf("----------------\n");
+    
+    //saida da carta 2//
+   
+    printf("\n------%s------\n",codigo2);
+
+    printf("Cidade: %s\n", nome2);
+
+    printf("Estado: %s\n",  estado2);
+
+    printf("Codigo: %s\n", codigo2);
+
+    printf("Populacao: %.2f de Habitantes ", populacao2);
+    printf("(%d)\n", populacao2>=populacao);
+
+    printf("Area: %.2fkm² " , area2);
+    printf("(%d)\n", area2>=area);
+
+    printf("PIB: %.2f Milhoes de reais ", PIB2);
+    printf("(%d)\n", PIB2>=PIB);
+
+    printf("Pontos Turisticos: %d ", pontosTuristicos2);
+    printf("(%d)\n", pontosTuristicos2>=pontosTuristicos);
+
+    printf("Densidade Demografica: %.2f hab/km² ", DesnsidadePopulacional2);
+    printf("(%d)\n", DesnsidadePopulacional2>=DensidadePopulacional);
+    
+    printf("PIB per capita: %.2f Reais por habitante ", pibPerCapita2);
+    printf("(%d)\n", pibPerCapita2>=pibPerCapita);
+
+    printf("Super Poder: ");
+    printf("(%d)\n", superPoder2>=superPoder);
+
+
+    printf("----------------\n");
+
+
     return 0;
 }
---
+++ SupertrunfoBasic/Testes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main () {
+    int a=1;
+    int b=3;
+    int carta_um_vence=1;
+    int carta_dois_vence=0; 
+
+    printf("%d",a>=b-carta_um_vence);
+    
+
+
+    return 0;
+}

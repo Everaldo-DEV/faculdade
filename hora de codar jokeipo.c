9c02e1cc2f0e03566ee5ed5dd8bf4d9a1a2527b7
Everaldo-DEV <sgt_erd@hotmail.com>
2025-10-30 11:11:27 -0300

    commit jkcpo

--- hora de codar jokeipo.c
+++ hora de codar jokeipo.c
@@ -16,39 +16,43 @@
         switch (escolhadojogador)
         {
         case 1:
-            printf("voce escolheu pedra \n");
+            printf("voce  pedra - ");
             break;
         case 2:
-            printf("voce escolheu papel \n");
+            printf("voce papel - ");
             break;
         case 3:
-            printf("voce escolheu tesoura \n");
+            printf("voce  tesoura - ");
             break;
         default:
             printf("opcao invalida \n");
             break;
+        }
 
         switch (escolhadocomputador)
         {
         case 1:
-            printf("computador escolheu pedra \n");
+            printf("computador  pedra \n");
             break;
         case 2:
-            printf("computador escolheu papel \n");
+            printf("computador papel \n");
             break;
         case 3:
-            printf("computador escolheu tesoura \n");
-            break;  
+            printf("computador tesoura \n");
+            break;
         }
-        if (escolhadocomputador==escolhadojogador){
-            printf("empate \n");
-        }
-        else if ((escolhadojogador==1 && escolhadocomputador==3) || 
-        (escolhadojogador==2 && escolhadocomputador==1) ||
-        (escolhadojogador==3 && escolhadocomputador==2))
+
+
+        if (escolhadocomputador==escolhadojogador)
+
+         {printf("O jogo empatou \n");}
+
+        else if ((escolhadojogador==1) && (escolhadocomputador==3) || 
+        (escolhadojogador==2) && (escolhadocomputador==1) ||
+        (escolhadojogador==3) && (escolhadocomputador==2))
         { printf("Voce venceu!! \n");}
         else 
         {printf("O computador venceu!! \n");}
         
     return 0;
-  }}
+  }
